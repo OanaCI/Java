4f2c9fd8df1f264bfef7877fc8f88fbe6ddb2a8c
OanaCI <oana.c.prodan@gmail.com>
2017-05-31 00:34:11 +0300

    update

--- src/main/java/com/training/playground/probleme/Array1.java
+++ src/main/java/com/training/playground/probleme/Array1.java
@@ -5,44 +5,55 @@ package com.training.playground.probleme;
  */
 public class Array1 {
     public static void main(String[] args) {
+//
+//        int[] a={23,45,56,12};
+////        parcurgerePara( a);
+////        parcurgereImpara( a);
+//        System.out.println( suma( a));
+//
+//
+//
+//
+//    }
+////    tipul returnat , nume , parametru si body
+//
+//    public static void parcurgerePara ( int[] a){
+//
+//        for(int i=0;i<a.length;i++){
+//            if(i%2==0){
+//                System.out.println(a[i]);
+//            }
+//        }
+//
+//    }
+//
+//
+//    public static void parcurgereImpara ( int[] a){
+//        for(int i=0;i<a.length;i++){
+//            if(i%2==1){
+//                System.out.println(a[i]*2);
+//            }
+//        }
+//    }
+//
+//    public static int suma ( int[] a){
+//        int sum=0;
+//
+//        for(int i=0;i<a.length;i++){
+//           sum=sum +a[i];
+//        }
+//        return sum;
 
-        int[] a={23,45,56,12};
-//        parcurgerePara( a);
-//        parcurgereImpara( a);
-        System.out.println( suma( a));
+
+        int[] a = {12, 23, 45, 56};
+
+      for ( int i=0 ;i<a.length;i++){
+          int poz=a.length-1-i;
+          System.out.println("pozitia" + poz + "are valoarea" + a[poz]);
+
+      }
 
 
-
-
-    }
-//    tipul returnat , nume , parametru si body
-
-    public static void parcurgerePara ( int[] a){
-
-        for(int i=0;i<a.length;i++){
-            if(i%2==0){
-                System.out.println(a[i]);
-            }
-        }
-
-    }
-
-
-    public static void parcurgereImpara ( int[] a){
-        for(int i=0;i<a.length;i++){
-            if(i%2==1){
-                System.out.println(a[i]*2);
-            }
-        }
-    }
-
-    public static int suma ( int[] a){
-        int sum=0;
-
-        for(int i=0;i<a.length;i++){
-           sum=sum +a[i];
-        }
-        return sum;
     }
 
 }
